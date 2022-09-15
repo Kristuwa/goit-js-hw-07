abb9b75c629afe86da9b86751b8e6b886e7bf50e
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2022-09-15 18:40:27 +0300

    add close modal press Escape

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -28,32 +28,31 @@ galleryListRef.addEventListener("click", onClickImage);
 
 function onClickImage(e) {
   e.preventDefault();
+
   if (e.target.nodeName !== "IMG") {
     return;
   }
 
   e.target.src = e.target.dataset.source;
 
+  createBasicLightbox(e.target);
+}
+
+function createBasicLightbox(elem) {
   const instance = basicLightbox.create(
     `
-  <div class="modal">
-  <img src='${e.target.src}' alt='${e.target.alt}' width="1280"/>
-  </div>
-`,
+	 <div class="modal">
+	 <img src='${elem.src}' alt='${elem.alt}' width="1280"/>
+	 </div>
+  `,
     {
-      closable: true,
-      className: "",
       onShow: (instance) => {
         instance
           .element()
           .querySelector("img")
           .addEventListener("click", instance.close);
 
-        window.addEventListener("keydown", (e) => {
-          if (e.code === "Escape") {
-            instance.close;
-          }
-        });
+        window.addEventListener("keydown", onPressEscape);
       },
 
       onClose: (instance) => {
@@ -61,9 +60,17 @@ function onClickImage(e) {
           .element()
           .querySelector("img")
           .removeEventListener("click", instance.close);
+
+        window.removeEventListener("keydown", onPressEscape);
       },
     }
   );
+
+  function onPressEscape(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
 
   instance.show();
 }
